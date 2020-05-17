f1bf94b69e45312e9195653ffddbd5d721413dad
Gerardo Puga <glpuga@gmail.com>
2020-05-17 16:34:14 -0300

    Project cleanup

---
+++ course/include/isometry/isometry.hpp
@@ -0,0 +1,25 @@
+/*
+ * Isometry library
+ * Agustin Alba Chicar, 2019
+ * Gerardo Puga, 2020
+ */
+
+#pragma once
+
+#include <cmath>
+#include <initializer_list>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+namespace ekumen {
+
+namespace math {
+
+/*
+ * your code here
+ */
+
+} // namespace math
+
+} // namespace ekumen
---
+++ course/src/isometry.cpp
@@ -0,0 +1,17 @@
+/*
+ * Isometry library
+ * Agustin Alba Chicar, 2019
+ * Gerardo Puga, 2020
+ */
+
+#include <isometry/isometry.hpp>
+
+namespace ekumen {
+namespace math {
+
+/*
+ * your code here
+ */
+
+} // namespace math
+} // namespace ekumen
